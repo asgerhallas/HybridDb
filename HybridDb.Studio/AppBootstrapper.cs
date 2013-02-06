0e7293c7540156531d6ecd421d52c790b72eb660
Lars Udengaard <lu@energysystems.dk>
2013-02-06 08:37:17 +0100

    Added studio project. Using caliburn.micro as MVVM framework

---
+++ HybridDb.Studio/AppBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Caliburn.Micro;
+using Castle.Windsor;
+using HybridDb.Studio.ViewModels;
+using System.Linq;
+
+namespace HybridDb.Studio
+{
+    public class AppBootstrapper : Bootstrapper<ShellViewModel>
+    {
+        WindsorContainer container;
+
+        protected override void Configure()
+        {
+            container = new WindsorContainer();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.Resolve(key, service);
+        }
+
+        protected override System.Collections.Generic.IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.ResolveAll(service).OfType<object>();
+        }
+    }
+}
---
+++ HybridDb.Studio/ViewModels/ShellViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Caliburn.Micro;
+
+namespace HybridDb.Studio.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+         
+    }
+}
