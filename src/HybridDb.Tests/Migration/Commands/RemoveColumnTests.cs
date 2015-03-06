31acd183e8a86b07ffa563fa3b34de92d42ea96d
Peter Bernth <phb@energysystems.dk>
2015-03-06 10:22:25 +0100

    RemoveColumn command

---
+++ src/HybridDb.Tests/Migration/Commands/RemoveColumnTests.cs
@@ -0,0 +1,32 @@
+using HybridDb.Config;
+using HybridDb.Migration.Commands;
+using Shouldly;
+using Xunit.Extensions;
+
+namespace HybridDb.Tests.Migration.Commands
+{
+    public class RemoveColumnTests : HybridDbTests
+    {
+        [Theory]
+        [InlineData(TableMode.UseTempTables)]
+        [InlineData(TableMode.UseRealTables)]
+        public void AddsColumn(TableMode mode)
+        {
+            Use(mode);
+            new CreateTable(new Table("Entities")).Execute(store);
+            new AddColumn("Entities", new Column("SomeColumn", typeof (int))).Execute(store);
+            
+            new RemoveColumn("Entities", new Column("SomeColumn", typeof (int))).Execute(store);
+
+            store.Schema.GetColumn("Entities", "SomeColumn").ShouldBe(null);
+        }
+
+        [Theory]
+        [InlineData("Document", true)]
+        [InlineData("OtherName", false)]
+        public void CommandSafetyDependsOnColumnName(string columnName, bool isUnsafe)
+        {
+            new RemoveColumn("Entities", new Column(columnName, typeof(int))).Unsafe.ShouldBe(isUnsafe);
+        }
+    }
+}
