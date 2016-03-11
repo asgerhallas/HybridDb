8e3e833bfca312a83392478daf281e4d69e6ccf3
Andreas <afk@energysystems.dk>
2016-03-11 15:27:52 +0100

    wip

--- src/HybridDb.Tests/Migrations/Commands/ChangeColumnTypeTests.cs
+++ src/HybridDb.Tests/Migrations/Commands/ChangeColumnTypeTests.cs
@@ -24,85 +24,106 @@ namespace HybridDb.Tests.Migrations.Commands
             store.Database.QuerySchema()["Entities"]["Col1"].Type.ShouldBe(typeof(string));
         }
 
-        //[Theory]
-        //[InlineData(TableMode.UseTempTables, typeof(int), false)]
-        //[InlineData(TableMode.UseTempDb, typeof(int), false)]
-        //[InlineData(TableMode.UseRealTables, typeof(int), false)]
-        //[InlineData(TableMode.UseTempTables, typeof(double), false)]
-        //[InlineData(TableMode.UseTempDb, typeof(double), false)]
-        //[InlineData(TableMode.UseRealTables, typeof(double), false)]
-        //[InlineData(TableMode.UseTempTables, typeof(string), true)]
-        //[InlineData(TableMode.UseTempDb, typeof(string), true)]
-        //[InlineData(TableMode.UseRealTables, typeof(string), true)]
-        //[InlineData(TableMode.UseTempTables, typeof(decimal), false)]
-        //[InlineData(TableMode.UseTempDb, typeof(decimal), false)]
-        //[InlineData(TableMode.UseRealTables, typeof(decimal), false)]
-        //public void ColumnIsOfCorrectType(TableMode mode, Type type, bool nullable)
-        //{
-        //    Use(TableMode.UseRealTables);
-        //    new CreateTable(new Table("Entities", new Column("Col1", typeof(int)))).Execute(store.Database);
+        [Theory]
+        [InlineData(TableMode.UseTempTables)]
+        [InlineData(TableMode.UseTempDb)]
+        [InlineData(TableMode.UseRealTables)]
+        public void ChangeTypeWithPrimaryKey(TableMode mode)
+        {
+            Use(mode);
+            UseTableNamePrefix(Guid.NewGuid().ToString());
+            new CreateTable(new Table("Entities", new Column("Col1", typeof(Guid)))).Execute(store.Database);
 
-        //    new AddColumn("Entities", new Column("Col2", type)).Execute(store.Database);
+            new ChangeColumnType("Entities", new Column("Col1", typeof(string), isPrimaryKey: true, length: 255)).Execute(store.Database);
 
-        //    store.Database.QuerySchema()["Entities"]["Col2"].Type.ShouldBe(type);
-        //    store.Database.QuerySchema()["Entities"]["Col2"].Nullable.ShouldBe(nullable);
-        //}
+            store.Database.QuerySchema()["Entities"]["Col1"].IsPrimaryKey.ShouldBe(true);
+        }
 
-        //[Theory]
-        //[InlineData(TableMode.UseTempTables)]
-        //[InlineData(TableMode.UseTempDb)]
-        //[InlineData(TableMode.UseRealTables)]
-        //public void SetsColumnAsNullableAndUsesUnderlyingTypeWhenNullable(TableMode mode)
-        //{
-        //    Use(mode);
-        //    UseTableNamePrefix(Guid.NewGuid().ToString());
-        //    new CreateTable(new Table("Entities", new Column("Col1", typeof(int)))).Execute(store.Database);
+        [Theory]
+        [InlineData(TableMode.UseTempTables, typeof(int), false)]
+        [InlineData(TableMode.UseTempDb, typeof(int), false)]
+        [InlineData(TableMode.UseRealTables, typeof(int), false)]
+        [InlineData(TableMode.UseTempTables, typeof(double), false)]
+        [InlineData(TableMode.UseTempDb, typeof(double), false)]
+        [InlineData(TableMode.UseRealTables, typeof(double), false)]
+        [InlineData(TableMode.UseTempTables, typeof(string), true)]
+        [InlineData(TableMode.UseTempDb, typeof(string), true)]
+        [InlineData(TableMode.UseRealTables, typeof(string), true)]
+        [InlineData(TableMode.UseTempTables, typeof(decimal), false)]
+        [InlineData(TableMode.UseTempDb, typeof(decimal), false)]
+        [InlineData(TableMode.UseRealTables, typeof(decimal), false)]
+        public void ColumnIsOfCorrectType(TableMode mode, Type type, bool nullable)
+        {
+            Use(TableMode.UseRealTables);
+            new CreateTable(new Table("Entities", new Column("Col1", typeof(int)))).Execute(store.Database);
 
-        //    new AddColumn("Entities", new Column("Col2", typeof(int?))).Execute(store.Database);
+            new ChangeColumnType("Entities", new Column("Col1", type)).Execute(store.Database);
 
-        //    store.Database.QuerySchema()["Entities"]["Col2"].Type.ShouldBe(typeof(int));
-        //    store.Database.QuerySchema()["Entities"]["Col2"].Nullable.ShouldBe(true);
-        //}
+            store.Database.QuerySchema()["Entities"]["Col1"].Type.ShouldBe(type);
+            store.Database.QuerySchema()["Entities"]["Col1"].Nullable.ShouldBe(nullable);
+        }
 
-        //[Theory]
-        //[InlineData(TableMode.UseTempTables)]
-        //[InlineData(TableMode.UseTempDb)]
-        //[InlineData(TableMode.UseRealTables)]
-        //public void CanSetColumnAsPrimaryKey(TableMode mode)
-        //{
-        //    Use(mode);
-        //    UseTableNamePrefix(Guid.NewGuid().ToString());
+        [Theory]
+        [InlineData(TableMode.UseTempTables)]
+        [InlineData(TableMode.UseTempDb)]
+        [InlineData(TableMode.UseRealTables)]
+        public void SetsColumnAsNullableAndUsesUnderlyingTypeWhenNullable(TableMode mode)
+        {
+            Use(mode);
+            UseTableNamePrefix(Guid.NewGuid().ToString());
+            new CreateTable(new Table("Entities", new Column("Col1", typeof(int)))).Execute(store.Database);
 
-        //    new CreateTable(new Table("Entities1", new Column("test", typeof(int)))).Execute(store.Database);
-        //    new AddColumn("Entities1", new Column("SomeInt", typeof(int), isPrimaryKey: true)).Execute(store.Database);
+            new ChangeColumnType("Entities", new Column("Col1", typeof(int?))).Execute(store.Database);
 
-        //    store.Database.QuerySchema()["Entities1"]["SomeInt"].IsPrimaryKey.ShouldBe(true);
-        //}
+            store.Database.QuerySchema()["Entities"]["Col1"].Type.ShouldBe(typeof(int));
+            store.Database.QuerySchema()["Entities"]["Col1"].Nullable.ShouldBe(true);
+        }
 
-        //[Theory]
-        //[InlineData(TableMode.UseTempTables)]
-        //[InlineData(TableMode.UseTempDb)]
-        //[InlineData(TableMode.UseRealTables)]
-        //public void CanAddColumnWithDefaultValue(TableMode mode)
-        //{
-        //    Use(mode);
-        //    UseTableNamePrefix(Guid.NewGuid().ToString());
-        //    new CreateTable(new Table("Entities1", new Column("test", typeof(int)))).Execute(store.Database);
+        [Theory]
+        [InlineData(TableMode.UseTempTables)]
+        [InlineData(TableMode.UseTempDb)]
+        [InlineData(TableMode.UseRealTables)]
+        public void CanSetColumnAsPrimaryKey(TableMode mode)
+        {
+            Use(mode);
+            UseTableNamePrefix(Guid.NewGuid().ToString());
 
-        //    new AddColumn("Entities1", new Column("SomeNullableInt", typeof(int?), defaultValue: null)).Execute(store.Database);
-        //    new AddColumn("Entities1", new Column("SomeOtherNullableInt", typeof(int?), defaultValue: 42)).Execute(store.Database);
-        //    new AddColumn("Entities1", new Column("SomeString", typeof(string), defaultValue: "peter")).Execute(store.Database);
-        //    new AddColumn("Entities1", new Column("SomeInt", typeof(int),  defaultValue: 666)).Execute(store.Database);
-        //    new AddColumn("Entities1", new Column("SomeDateTime", typeof(DateTime),  defaultValue: new DateTime(1999, 12, 24))).Execute(store.Database);
+            new CreateTable(new Table("Entities1", new Column("col1", typeof(int)))).Execute(store.Database);
+            new ChangeColumnType("Entities1", new Column("col1", typeof(int), isPrimaryKey: true)).Execute(store.Database);
 
-        //    var schema = store.Database.QuerySchema();
+            store.Database.QuerySchema()["Entities1"]["col1"].IsPrimaryKey.ShouldBe(true);
+        }
 
-        //    schema["Entities1"]["SomeNullableInt"].DefaultValue.ShouldBe(null);
-        //    schema["Entities1"]["SomeOtherNullableInt"].DefaultValue.ShouldBe(42);
-        //    schema["Entities1"]["SomeString"].DefaultValue.ShouldBe("peter");
-        //    schema["Entities1"]["SomeInt"].DefaultValue.ShouldBe(666);
-        //    schema["Entities1"]["SomeDateTime"].DefaultValue.ShouldBe(new DateTime(1999, 12, 24));
-        //}
+        [Theory]
+        [InlineData(TableMode.UseTempTables)]
+        [InlineData(TableMode.UseTempDb)]
+        [InlineData(TableMode.UseRealTables)]
+        public void CanAddColumnWithDefaultValue(TableMode mode)
+        {
+            Use(mode);
+            UseTableNamePrefix(Guid.NewGuid().ToString());
+            new CreateTable(new Table("Entities1", new Column("test", typeof(int)))).Execute(store.Database);
+
+            new AddColumn("Entities1", new Column("SomeNullableInt", typeof(int))).Execute(store.Database);
+            new AddColumn("Entities1", new Column("SomeOtherNullableInt", typeof(int))).Execute(store.Database);
+            new AddColumn("Entities1", new Column("SomeString", typeof(int))).Execute(store.Database);
+            new AddColumn("Entities1", new Column("SomeInt", typeof(int))).Execute(store.Database);
+            new AddColumn("Entities1", new Column("SomeDateTime", typeof(DateTime))).Execute(store.Database);
+
+            new ChangeColumnType("Entities1", new Column("SomeNullableInt", typeof(int?), defaultValue: null)).Execute(store.Database);
+            new ChangeColumnType("Entities1", new Column("SomeOtherNullableInt", typeof(int?), defaultValue: 42)).Execute(store.Database);
+            new ChangeColumnType("Entities1", new Column("SomeString", typeof(string), defaultValue: "peter")).Execute(store.Database);
+            new ChangeColumnType("Entities1", new Column("SomeInt", typeof(int), defaultValue: 666)).Execute(store.Database);
+            new ChangeColumnType("Entities1", new Column("SomeDateTime", typeof(DateTime), defaultValue: new DateTime(1999, 12, 24))).Execute(store.Database);
+
+            var schema = store.Database.QuerySchema();
+
+            schema["Entities1"]["SomeNullableInt"].DefaultValue.ShouldBe(null);
+            schema["Entities1"]["SomeOtherNullableInt"].DefaultValue.ShouldBe(42);
+            schema["Entities1"]["SomeString"].DefaultValue.ShouldBe("peter");
+            schema["Entities1"]["SomeInt"].DefaultValue.ShouldBe(666);
+            schema["Entities1"]["SomeDateTime"].DefaultValue.ShouldBe(new DateTime(1999, 12, 24));
+        }
 
         //[Fact(Skip = "Not solved yet")]
         //public void ShouldNotAllowSqlInjection()
@@ -113,16 +134,16 @@ namespace HybridDb.Tests.Migrations.Commands
         //    store.Database.QuerySchema().ShouldContainKey("Entities1");
         //}
 
-        [Fact]
-        public void IsSafe()
-        {
-            new ChangeColumnType("Entities", new Column("Col", typeof(int))).Unsafe.ShouldBe(true);
-        }
+        //[Fact]
+        //public void IsSafe()
+        //{
+        //    new ChangeColumnType("Entities", new Column("Col", typeof(int))).Unsafe.ShouldBe(true);
+        //}
 
-        [Fact]
-        public void RequiresReprojection()
-        {
-            new AddColumn("Entities", new Column("Col", typeof(int))).RequiresReprojectionOf.ShouldBe(null);
-        }
+        //[Fact]
+        //public void RequiresReprojection()
+        //{
+        //    new AddColumn("Entities", new Column("Col", typeof(int))).RequiresReprojectionOf.ShouldBe(null);
+        //}
     }
 }
--- src/HybridDb/Migrations/Commands/ChangeColumnType.cs
+++ src/HybridDb/Migrations/Commands/ChangeColumnType.cs
@@ -1,30 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using HybridDb.Config;
 
 namespace HybridDb.Migrations.Commands
 {
     public class ChangeColumnType : SchemaMigrationCommand
     {
-        public ChangeColumnType(string tablename, Column column)
+        static readonly IEnumerable<string> constraintQueries = new List<string>
         {
-            Tablename = tablename;
+            @"
+                SELECT default_constraints.name
+                FROM sys.all_columns
+                INNER JOIN sys.tables ON all_columns.object_id = tables.object_id
+                INNER JOIN sys.schemas ON tables.schema_id = schemas.schema_id
+                INNER JOIN sys.default_constraints ON all_columns.default_object_id = default_constraints.object_id
+                WHERE tables.name = @TableName AND all_columns.name = @ColumnName",
+            @"
+                SELECT ct.constraint_name
+                FROM information_schema.table_constraints AS ct
+                JOIN information_schema.key_column_usage AS k
+                ON ct.table_name = k.table_name
+                AND ct.constraint_catalog = k.constraint_catalog
+                AND ct.constraint_schema = k.constraint_schema 
+                AND ct.constraint_name = k.constraint_name
+                WHERE ct.constraint_type = 'primary key'
+                AND k.table_name = @TableName
+                AND k.column_name = @ColumnName"
+        }; 
+
+        static readonly IEnumerable<string> tempTablesConstraintQueries = new List<string>
+        {
+            @"
+                SELECT c.name
+                FROM tempdb.dbo.sysobjects AS t
+                INNER JOIN tempdb.dbo.sysobjects AS c ON c.parent_obj = t.id AND c.type = 'D'
+                WHERE t.id = OBJECT_ID('tempdb.dbo.' + @TableName) AND c.name LIKE ('%__' + SUBSTRING(@ColumnName, 1, 5) + '__%')",
+            @"
+                SELECT k.constraint_name
+                FROM tempdb.dbo.sysobjects AS t
+                INNER JOIN tempdb.information_schema.table_constraints AS ct ON ct.table_name = t.name
+                INNER JOIN tempdb.information_schema.key_column_usage AS k
+                ON ct.table_name = k.table_name
+                AND ct.constraint_catalog = k.constraint_catalog
+                AND ct.constraint_schema = k.constraint_schema 
+                AND ct.constraint_name = k.constraint_name
+                WHERE ct.constraint_type = 'primary key'
+                AND t.id = OBJECT_ID('tempdb.dbo.' + @TableName)
+                AND k.column_name = @ColumnName"
+        }; 
+
+        public ChangeColumnType(string tableName, Column column)
+        {
+            TableName = tableName;
             Column = column;
         }
 
-        public string Tablename { get; }
+        public string TableName { get; }
         public Column Column { get; }
 
         public override void Execute(IDatabase database)
         {
-            var sql = new SqlBuilder();
-            sql.Append("alter table {0} alter column {1}", database.FormatTableNameAndEscape(Tablename), database.Escape(Column.Name));
-            sql.Append(GetColumnSqlType(Column));
+            var builder = new SqlBuilder();
+            var formattedAndEscapedTableName = database.FormatTableNameAndEscape(TableName);
+            var escapedColumnName = database.Escape(Column.Name);
 
-            database.RawExecute(sql.ToString());
+            //DROP CONSTRAINTS
+            foreach (var constraintName in (database is SqlServerUsingTempTables ? tempTablesConstraintQueries : constraintQueries)
+                .Select(x => database.RawQuery<string>(x, new { TableName = database.FormatTableName(TableName), ColumnName = Column.Name }))
+                .Select(x => x.SingleOrDefault())
+                .Where(x => x != null))
+            {
+                builder.Append("ALTER TABLE {0} DROP CONSTRAINT {1};", formattedAndEscapedTableName, database.Escape(constraintName));
+            }
+
+            
+            builder
+                //ALTER TABLE
+                .Append(GetAlterTableStatement(formattedAndEscapedTableName, escapedColumnName))
+
+                //DEFAULT CONSTRAINT
+                .Append(Column.DefaultValue != null, "ALTER TABLE {0} ADD DEFAULT '{1}' FOR {2};", formattedAndEscapedTableName, Column.DefaultValue, escapedColumnName)
+
+                //PRIMAY KEY
+                .Append(Column.IsPrimaryKey, @"ALTER TABLE {0} ADD PRIMARY KEY ({1});", formattedAndEscapedTableName, escapedColumnName);
+
+            database.RawExecute(builder.ToString());
+        }
+
+        string GetAlterTableStatement(string formattedAndEscapedTableName, string escapedColumnName)
+        {
+            var builder = new SqlBuilder();
+            var sqlColumn = SqlTypeMap.Convert(Column);
+
+            builder
+                .Append("ALTER TABLE {0} ALTER COLUMN {1}", formattedAndEscapedTableName, escapedColumnName)
+                .Append(new SqlParameter {DbType = sqlColumn.DbType}.SqlDbType.ToString())
+                .Append(sqlColumn.Length != null, "(" + sqlColumn.Length + ")")
+                .Append(Column.Nullable, "NULL").Or("NOT NULL")
+                .Append(";");
+
+            return builder.ToString();
         }
 
         public override string ToString()
         {
-            return $"Change type of column {Column} on table {Tablename} to {Column.Type}.";
+            return $"Change type of column {Column} on table {TableName} to {Column.Type}.";
         }
     }
 }
