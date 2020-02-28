4e29d6606a867e7e8aecf58e6b403b37a81ad8e7
Asger Hallas <ahl@asgerhallas.dk>
2020-02-28 19:02:09 +0100

    New connection string

--- src/HybridDb.Tests/HybridDbTests.cs
+++ src/HybridDb.Tests/HybridDbTests.cs
@@ -64,7 +64,7 @@ namespace HybridDb.Tests
             var isAppveyor = Environment.GetEnvironmentVariable("APPVEYOR") != null;
 
             return isAppveyor
-                ? "Server=(local)\\SQL2014;Database=master;User ID=sa;Password=Password12!"
+                ? "Server=(local)\\SQL2017;Database=master;User ID=sa;Password=Password12!"
                 : "Data Source=.; Integrated Security=True";
         }
 
