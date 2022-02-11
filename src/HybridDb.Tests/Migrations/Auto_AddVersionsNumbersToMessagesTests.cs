655abd1142920d46900d9c988d0e5204bd4386d1
asgerhallas <ahl@asgerhallas.dk>
2022-02-11 12:52:02 +0100

    Remove unused code

--- src/HybridDb.Tests/Migrations/Auto_AddVersionsNumbersToMessagesTests.cs
+++ src/HybridDb.Tests/Migrations/Auto_AddVersionsNumbersToMessagesTests.cs
@@ -61,37 +61,5 @@ namespace HybridDb.Tests.Migrations
         }
 
         public record Message() : HybridDbMessage(Guid.NewGuid().ToString(), "default");
-
-        public class CreateQueueTable_v1 : DdlCommand
-        {
-            public QueueTable QueueTable { get; }
-
-            public CreateQueueTable_v1(QueueTable queueTable)
-            {
-                Safe = true;
-                QueueTable = queueTable;
-            }
-
-            public override string ToString() => "Create queue table";
-
-            public override void Execute(DocumentStore store)
-            {
-                var tableName = store.Database.FormatTableName(QueueTable.Name);
-
-                store.Database.RawExecute($@"
-                    if (object_id('{tableName}', 'U') is null)
-                    begin
-                        CREATE TABLE [dbo].[{tableName}] (
-                            [Topic] [nvarchar](850) NOT NULL,
-	                        [Id] [nvarchar](850) NOT NULL,
-	                        [CommitId] [uniqueidentifier] NOT NULL,
-	                        [Discriminator] [nvarchar](850) NOT NULL,
-	                        [Message] [nvarchar](max) NULL,
-
-                            CONSTRAINT [PK_{tableName}] PRIMARY KEY CLUSTERED ([Topic] ASC, [Id] ASC)
-                        )
-                    end", schema: true);
-            }
-        }
     }
 }
