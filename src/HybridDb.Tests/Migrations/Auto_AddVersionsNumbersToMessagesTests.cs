cfd37dad42f9d38a6598a568b9c44e2be822258b
asgerhallas <ahl@asgerhallas.dk>
2022-02-11 12:51:39 +0100

    Add test for auto migration of message queue table

---
+++ src/HybridDb.Tests/Migrations/Auto_AddVersionsNumbersToMessagesTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using HybridDb.Migrations.Schema;
+using HybridDb.Queue;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace HybridDb.Tests.Migrations
+{
+    public class Auto_AddVersionsNumbersToMessagesTests : HybridDbTests
+    {
+        public Auto_AddVersionsNumbersToMessagesTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void AutoMigratesMessageQueueTable()
+        {
+            var tablename = "messages";
+            var tableNameFormatted = store.Database.FormatTableName(tablename);
+
+            store.Database.RawExecute($@"
+                if (object_id('{tableNameFormatted}', 'U') is null)
+                begin
+                    CREATE TABLE [dbo].[{tableNameFormatted}] (
+                        [Topic] [nvarchar](850) NOT NULL,
+	                    [Id] [nvarchar](850) NOT NULL,
+	                    [CommitId] [uniqueidentifier] NOT NULL,
+	                    [Discriminator] [nvarchar](850) NOT NULL,
+	                    [Message] [nvarchar](max) NULL,
+
+                        CONSTRAINT [PK_{tableNameFormatted}] PRIMARY KEY CLUSTERED ([Topic] ASC, [Id] ASC)
+                    )
+                end", schema: true);
+
+            store.Database.RawExecute(@$"
+                insert into {store.Database.FormatTableNameAndEscape(tablename)} 
+                (Topic, Id, CommitId, Discriminator, Message)
+                values (@Topic, @Id, @CommitId, @Discriminator, @Message);",
+                new
+                {
+                    Topic = "default",
+                    Id = Guid.NewGuid().ToString(),
+                    CommitId = Guid.NewGuid().ToString(),
+                    Discriminator = configuration.TypeMapper.ToDiscriminator(typeof(Message)),
+                    Message = configuration.Serializer.Serialize(new Message())
+                });
+
+            ResetConfiguration();
+
+            configuration.UseMessageQueue(new MessageQueueOptions
+            {
+                Version = new Version("1.0")
+            });
+
+            TouchStore();
+
+            var message = store.Execute(new DequeueCommand(new QueueTable(tablename), new[] {"default"}));
+
+            message.ShouldNotBe(null);
+        }
+
+        public record Message() : HybridDbMessage(Guid.NewGuid().ToString(), "default");
+
+        public class CreateQueueTable_v1 : DdlCommand
+        {
+            public QueueTable QueueTable { get; }
+
+            public CreateQueueTable_v1(QueueTable queueTable)
+            {
+                Safe = true;
+                QueueTable = queueTable;
+            }
+
+            public override string ToString() => "Create queue table";
+
+            public override void Execute(DocumentStore store)
+            {
+                var tableName = store.Database.FormatTableName(QueueTable.Name);
+
+                store.Database.RawExecute($@"
+                    if (object_id('{tableName}', 'U') is null)
+                    begin
+                        CREATE TABLE [dbo].[{tableName}] (
+                            [Topic] [nvarchar](850) NOT NULL,
+	                        [Id] [nvarchar](850) NOT NULL,
+	                        [CommitId] [uniqueidentifier] NOT NULL,
+	                        [Discriminator] [nvarchar](850) NOT NULL,
+	                        [Message] [nvarchar](max) NULL,
+
+                            CONSTRAINT [PK_{tableName}] PRIMARY KEY CLUSTERED ([Topic] ASC, [Id] ASC)
+                        )
+                    end", schema: true);
+            }
+        }
+    }
+}
--- src/HybridDb/Queue/QueueTable.cs
+++ src/HybridDb/Queue/QueueTable.cs
@@ -8,7 +8,7 @@ namespace HybridDb.Queue
     {
         public QueueTable(string name) : base(name,
             new Column<string>("Topic", length: 850, nullable: false),
-            new Column<string>("Version", length: 40, nullable: false),
+            new Column<string>("Version", length: 40, nullable: false, defaultValue: "0.0"),
             new Column<string>("Id", length: 850, nullable: false),
             new Column<Guid>("CommitId"),
             new Column<string>("Discriminator", length: 850, nullable: false),
