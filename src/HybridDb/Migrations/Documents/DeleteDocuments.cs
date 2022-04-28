ba3f2998854c6360a742a052246452d02df8c5c1
asgerhallas <ahl@asgerhallas.dk>
2022-04-28 12:51:00 +0200

    Rename DeleteDocument to DeleteDocuments to avoid confusion

--- src/HybridDb/Migrations/Documents/DeleteDocuments.cs
+++ src/HybridDb/Migrations/Documents/DeleteDocuments.cs
@@ -3,9 +3,9 @@ using HybridDb.Config;
 
 namespace HybridDb.Migrations.Documents
 {
-    public class DeleteDocument<T> : DocumentRowMigrationCommand
+    public class DeleteDocuments<T> : DocumentRowMigrationCommand
     {
-        public DeleteDocument(params IDocumentMigrationMatcher[] matchers) : base(typeof(T), matchers) { }
+        public DeleteDocuments(params IDocumentMigrationMatcher[] matchers) : base(typeof(T), matchers) { }
 
         public override IDictionary<string, object> Execute(IDocumentSession session, ISerializer serializer, IDictionary<string, object> row)
         {
