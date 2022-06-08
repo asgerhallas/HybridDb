73d34eeee6bc272be9bdc56b72da4060a0275544
asgerhallas <ahl@asgerhallas.dk>
2022-06-08 10:07:59 +0200

    Add guards against enqueuing the envelope as the message

--- src/HybridDb/Queue/QueueEx.cs
+++ src/HybridDb/Queue/QueueEx.cs
@@ -23,23 +23,29 @@ namespace HybridDb.Queue
                     .Else(() => decoratee(tx, command)()));
         }
 
-        public static void Enqueue(this IDocumentSession session, object message, string topic = null) => 
-            Enqueue(session, Guid.NewGuid().ToString(), message, topic);
+        public static void Enqueue(this IDocumentSession session, object message, string topic = null) =>
+            Enqueue(session, new HybridDbMessage(Guid.NewGuid().ToString(), message, topic));
 
-        public static void Enqueue(this IDocumentSession session, string id, object message, string topic = null)
-        {
-            var queueTable = session.Advanced.DocumentStore.Configuration.Tables.Values.OfType<QueueTable>().Single();
-
-            session.Advanced.Defer(new EnqueueCommand(queueTable, new HybridDbMessage(id, message, topic)));
-        }
+        public static void Enqueue(this IDocumentSession session, string id, object message, string topic = null) => 
+            Enqueue(session, new HybridDbMessage(id, message, topic));
 
         public static void Enqueue<T>(this IDocumentSession session, Func<T, Guid, string> idGenerator, T message, string topic = null)
         {
-            var queueTable = session.Advanced.DocumentStore.Configuration.Tables.Values.OfType<QueueTable>().Single();
-
             string IdGenerator(object p, Guid etag) => idGenerator((T)p, etag);
 
-            session.Advanced.Defer(new EnqueueCommand(queueTable, new HybridDbMessage(Guid.NewGuid().ToString(), message, topic), IdGenerator));
+            Enqueue(session, new HybridDbMessage(Guid.NewGuid().ToString(), message, topic), IdGenerator);
+        }
+
+        public static void Enqueue(this IDocumentSession session, HybridDbMessage message, Func<object, Guid, string> idGenerator = null)
+        {
+            if (message.Payload is HybridDbMessage)
+            {
+                throw new ArgumentException("Enqueued message must not be of type HybridDbMessage.");
+            }
+
+            var queueTable = session.Advanced.DocumentStore.Configuration.Tables.Values.OfType<QueueTable>().Single();
+
+            session.Advanced.Defer(new EnqueueCommand(queueTable, message, idGenerator));
         }
     }
 }
