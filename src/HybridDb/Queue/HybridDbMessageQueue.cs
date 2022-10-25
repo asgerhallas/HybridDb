7ad5f3ba816869d99e4acd60dc441fd42fd8aa4b
asgerhallas <ahl@asgerhallas.dk>
2022-10-25 15:05:15 +0200

    Add convenience return values for testing

--- src/HybridDb/Queue/HybridDbMessageQueue.cs
+++ src/HybridDb/Queue/HybridDbMessageQueue.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
--- src/HybridDb/Queue/QueueEx.cs
+++ src/HybridDb/Queue/QueueEx.cs
@@ -26,21 +26,21 @@ namespace HybridDb.Queue
                     .Else(() => decoratee(tx, command)));
         }
 
-        public static void Enqueue(this IDocumentSession session, object message, string topic = null, Dictionary<string, string> metadata = null)
+        public static HybridDbMessage Enqueue(this IDocumentSession session, object message, string topic = null, Dictionary<string, string> metadata = null)
         {
             if (message == null) throw new ArgumentNullException(nameof(message));
 
-            Enqueue(session, new HybridDbMessage(Guid.NewGuid().ToString(), message, topic, metadata));
+            return Enqueue(session, new HybridDbMessage(Guid.NewGuid().ToString(), message, topic, metadata));
         }
 
-        public static void Enqueue(this IDocumentSession session, string id, object message, string topic = null, Dictionary<string, string> metadata = null)
+        public static HybridDbMessage Enqueue(this IDocumentSession session, string id, object message, string topic = null, Dictionary<string, string> metadata = null)
         {
             if (message == null) throw new ArgumentNullException(nameof(message));
 
-            Enqueue(session, new HybridDbMessage(id, message, topic, metadata));
+            return Enqueue(session, new HybridDbMessage(id, message, topic, metadata));
         }
 
-        public static void Enqueue<T>(this IDocumentSession session, Func<T, Guid, string> idGenerator, T message, string topic = null, Dictionary<string, string> metadata = null)
+        public static HybridDbMessage Enqueue<T>(this IDocumentSession session, Func<T, Guid, string> idGenerator, T message, string topic = null, Dictionary<string, string> metadata = null)
         {
             if (idGenerator == null) throw new ArgumentNullException(nameof(idGenerator));
             if (message == null) throw new ArgumentNullException(nameof(message));
@@ -49,10 +49,10 @@ namespace HybridDb.Queue
 
             var envelope = new HybridDbMessage(Guid.NewGuid().ToString(), message, topic, metadata);
 
-            Enqueue(session, envelope, IdGenerator);
+            return Enqueue(session, envelope, IdGenerator);
         }
 
-        public static void Enqueue(this IDocumentSession session, HybridDbMessage message, Func<object, Guid, string> idGenerator = null)
+        public static HybridDbMessage Enqueue(this IDocumentSession session, HybridDbMessage message, Func<object, Guid, string> idGenerator = null)
         {
             if (message == null) throw new ArgumentNullException(nameof(message));
 
@@ -66,15 +66,17 @@ namespace HybridDb.Queue
             var queueTable = session.Advanced.DocumentStore.Configuration.Tables.Values.OfType<QueueTable>().Single();
 
             session.Advanced.Defer(new EnqueueCommand(queueTable, message, idGenerator));
+
+            return message;
         }
 
         static string GetNextCorrelationIds(IDocumentSession session, HybridDbMessage message)
         {
             if (session.Advanced.SessionData.TryGetValue(MessageContext.Key, out var value) &&
                 value is MessageContext messageContext && 
-                messageContext.IncomingMessage.Metadata.TryGetValue(HybridDbMessage.CorrelationIdsKey, out var currentCorrelationIds))
+                messageContext.IncomingMessage.Metadata.TryGetValue(HybridDbMessage.CorrelationIdsKey, out var correlationIds))
             {
-                var nextCorrelationIds = JArray.Parse(currentCorrelationIds);
+                var nextCorrelationIds = JArray.Parse(correlationIds);
                 nextCorrelationIds.Add(message.Id);
 
                 return nextCorrelationIds.ToString();
