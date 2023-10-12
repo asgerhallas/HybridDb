03543ef44b6c746963d53a653f9d3b946145bfec
KamillaFincke <kasf@nrgisystems.dk>
2023-10-12 15:00:13 +0200

    fix to adding indices

--- src/HybridDb.Tests/Migrations/Commands/AddMigrationIndicesTests.cs
+++ src/HybridDb.Tests/Migrations/Commands/AddMigrationIndicesTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using HybridDb.Config;
 using HybridDb.Migrations.Schema.Commands;
 using HybridDb.Queue;
+using Microsoft.SqlServer.Management.Smo;
 using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
@@ -48,11 +49,16 @@ namespace HybridDb.Tests.Migrations.Commands
         {
             Use(mode);
             UseTableNamePrefix(Guid.NewGuid().ToString());
+            configuration.UseMessageQueue();
+            Document<Entity>();
 
             store.Execute(new CreateTable(new QueueTable("QueueTable")));
+            store.Execute(new CreateTable(new DocumentTable("Entities")));
+
             store.Execute(new AddMigrationIndices());
 
             var queueTable = new QueueTable("QueueTable");
+            var documentTable = new DocumentTable("Entities");
 
             var queueTableIndexQuery = store.Database
                 .RawQuery<object>($"sp_helpindex '{store.Database.FormatTableName(queueTable.Name)}'")
@@ -61,6 +67,17 @@ namespace HybridDb.Tests.Migrations.Commands
             var queueTableIndices = queueTableIndexQuery
                 .Select(x => (x["index_name"], x["index_keys"]))
                 .ToList();
+
+            var documentTableIndexQuery = store.Database
+                .RawQuery<object>($"sp_helpindex '{store.Database.FormatTableName(documentTable.Name)}'")
+                .Cast<IDictionary<string, object>>();
+
+            var documentTableIndices = documentTableIndexQuery 
+                .Select(x => (x["index_name"], x["index_keys"]))
+                .ToList();
+
+            documentTableIndices .ShouldContain(("idx_Version", "Version"));
+            documentTableIndices .ShouldContain(("idx_AwaitsReprojection", "AwaitsReprojection"));
 
             queueTableIndices.ShouldNotContain(("idx_Version", "Version"));
             queueTableIndices.ShouldNotContain(("idx_AwaitsReprojection", "AwaitsReprojection"));
