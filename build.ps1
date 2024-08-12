af9c0b2f9f1e9c21206d8b6b2c7764b8e74f656e
asgerhallas <ahl@asgerhallas.dk>
2024-08-12 11:07:22 +0200

    add scripts

---
+++ build.ps1
@@ -0,0 +1,9 @@
+param (
+    [Parameter(Mandatory=$true)][string]$Version
+)
+
+Import-Module .\functions.ps1 -Force
+
+Run "Build" { dotnet build -p:Version=$Version } 
+Run "Test" { dotnet test --no-build --logger:"console;verbosity=normal" }
+Run "Pack" { dotnet pack src/HybridDb/ -c Release --include-symbols -p:SymbolPackageFormat=snupkg -p:Version=$Version }
