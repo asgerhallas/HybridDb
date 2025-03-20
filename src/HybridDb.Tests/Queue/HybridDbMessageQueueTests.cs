1edd91dcf9be9ef63eff0b1a50ef364a27a3358f
asgerhallas <ahl@asgerhallas.dk>
2025-03-20 09:39:08 +0100

    clean up using´

--- src/HybridDb.Tests/Queue/HybridDbMessageQueueTests.cs
+++ src/HybridDb.Tests/Queue/HybridDbMessageQueueTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Reactive.Threading.Tasks;
