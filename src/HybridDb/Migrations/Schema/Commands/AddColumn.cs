5ee72038efe501d3ecb0f94fc9e0103cce0395ce
Mathias Munk Hansen <hiha@nrgisystems.dk>
2025-12-03 15:10:02 +0100

    Use explicit schema migration + longer timeout

--- src/HybridDb/Migrations/Schema/Commands/AddColumn.cs
+++ src/HybridDb/Migrations/Schema/Commands/AddColumn.cs
@@ -18,12 +18,13 @@ namespace HybridDb.Migrations.Schema.Commands
 
         public override string ToString() => $"Add column {Column} to table {Tablename}.";
 
-        public override void Execute(DocumentStore store)
-        {
-            store.Database.RawExecute(new SqlBuilder()
-                .Append($"alter table {store.Database.FormatTableNameAndEscape(Tablename)} add {store.Database.Escape(Column.Name)}")
-                .Append(DdlCommandEx.BuildColumnSql(Column))
-                .ToString());
-        }
+        public override void Execute(DocumentStore store) =>
+            store.Database.RawExecute(
+                new SqlBuilder()
+                    .Append($"alter table {store.Database.FormatTableNameAndEscape(Tablename)} add {store.Database.Escape(Column.Name)}")
+                    .Append(DdlCommandEx.BuildColumnSql(Column))
+                    .ToString(),
+                schema: true,
+                commandTimeout: 300);
     }
 }
